42d72a540ff9ac417f5890507b7a04dc3b3cfa84
vlad <vlad@DESKTOP-K3EMTQ0>
2024-10-20 18:15:14 +0300

    Add project files.

---
+++ Vlad_Crisan_Lab2/Data/Vlad_Crisan_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vlad_Crisan_Lab2.Models;
+
+namespace Vlad_Crisan_Lab2.Data
+{
+    public class Vlad_Crisan_Lab2Context : DbContext
+    {
+        public Vlad_Crisan_Lab2Context (DbContextOptions<Vlad_Crisan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vlad_Crisan_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Vlad_Crisan_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
